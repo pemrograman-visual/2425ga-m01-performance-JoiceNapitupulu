5db79f1e6bf96b0c053f93764789ac06f391f688
ArionManurung <anastasyajoicen@gmail.com>
2024-10-18 11:12:06 +0700

    update

--- M01.java
+++ M01.java
@@ -13,6 +13,13 @@ public class M01 {
 
         kredit1 = 0;
         kredit = input.nextLine();
+        a = 4.0;
+        aB = 3.5;
+        b = 3.0;
+        bC = 2.5;
+        c = 2.0;
+        d = 1.0;
+        e = 0.0;
         while (!kredit.equals("---")) {
             if (kredit.equals("---")) {
                 kredit = kredit;
